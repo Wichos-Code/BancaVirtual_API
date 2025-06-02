ddfade5a255daaab19a2fd0402383980093123fb
MR-APEN <aleapen82@gmail.com>
2025-06-02 16:53:00 -0600

    build: create server with principal settings

---
+++ configs/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import { dbConnection } from "./mongo.js";
+import { swaggerDocs, swaggerUi } from "./swagger.js";
+
+const middlewares = (app) => {
+    app.use(express.urlencoded({extended: false}))
+    app.use(express.json())
+    app.use(cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }));
+    app.use(helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                scriptSrc: ["'self'", "'unsafe-inline'", `http://localhost:${process.env.PORT}`],
+                connectSrc: ["'self'", `http://localhost:${process.env.PORT}`],
+                imgSrc: ["'self'", "data:"],
+                styleSrc: ["'self'", "'unsafe-inline'"],
+            },
+        },
+    }));
+    app.use(morgan("dev"))
+
+}
+
+const routes = (app) => {
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+}
+
+const connectDB = async () => {
+    try {
+        await dbConnection()
+    } catch (err) {
+        console.log(`Database connection failed ${err}`)
+        process.exit(1)
+    }
+}
+
+export const initServer = () => {
+    const app = express()
+    try {
+        middlewares(app)
+        connectDB()
+        routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Server running on port ${process.env.PORT}`)
+    } catch (err) {
+        console.log(`Server init failed ${err}`)
+    }
+}
---
+++ configs/swagger.js
@@ -0,0 +1,29 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express"
+
+const swaggerOptions = {
+    swaggerDefinition:{
+        openapi: "3.0.0",
+        info:{
+            title:"Banca Virtual API",
+            version:"1.0.0",
+            description:"API para el entorno de una banca en línea",
+            contact:{
+                name:"ByteForces",
+                email:"ByteForce@kinal.edu.gt"
+            }
+        },
+        servers:[
+            {
+                url:"http://127.0.0.1:3000/virtualBank/v1"
+            }
+        ]
+    },
+    apis:[
+        "./src/auth/*.js",
+    ]
+}
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions)
+
+export { swaggerDocs, swaggerUi }
