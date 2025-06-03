4ee14c3fa9c44c4e4090f365dc4ad08e92187e41
Josue Garcia <jgarcia-2023324@kinal.edu.gt>
2025-06-03 16:51:00 -0600

    feat: account model added

---
+++ src/account/account.model.js
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+
+const accountSchema = new Schema({
+  noAccount: {
+    type: Number,
+    maxLength: [25, "Account number cannot exceed 25 characters"],
+  },
+  currency: {
+    type: String,
+    required: true,
+    enum: ["USD", "EUR", "GTQ", "MXN", "COP", "ARS", "JPY", "GBP"], 
+  },
+  amount: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ["Monetary", "Savings"],
+  },
+  status: {
+    type: Boolean,
+    default: false,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+}, {
+  versionKey: false,
+  timestamps: true 
+});
+
+export default model("Account", accountSchema);
--- src/helpers/db-validators.js
+++ src/helpers/db-validators.js
@@ -1,4 +1,5 @@
 import User from "../user/user.model.js"
+import Account from "../account/account.model.js"
 
 export const emailExist = async(email = '') =>{
     const existe = await User.findOne({email})
@@ -11,5 +12,12 @@ export const usernameExist = async(username = '') =>{
     const existe = await User.findOne({username})
     if(existe){
         throw new Error(`El email ${username} ya fue registrado previamente`)
+    }
+}
+
+export const accountExist = async(noAccount = '') => {
+    const existe = await Account.findOne({noAccount})
+    if (existe) {
+        throw new Error(`La cuenta ${noAccount} ya fue registrado previamente`)
     }
 }
